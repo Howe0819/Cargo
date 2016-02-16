43ba4d18a7f397a9a18721abdb4874ab6d8eefc1
Liu Junling <liujunling1202@gmail.com>
2016-02-17 00:00:16 +0100

    oh

---
+++ app/src/main/java/resultList.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Junling on 16/2/16.
+ */
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.example.vin_s.cargo.R;
+
+public class resultList extends AppCompatActivity{
+
+}
+
